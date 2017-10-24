46298b9b7d61d253dc0301afe870e1066b5fb9e8
jacobruleaux <jakeruleaux@gmail.com>
2017-10-24 16:58:51 -0700

    generate albumlist comp

--- App.js
+++ App.js
@@ -3,42 +3,15 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Albums!
-        </Text>
+export default App = () => (
+      <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
       </View>
     );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
