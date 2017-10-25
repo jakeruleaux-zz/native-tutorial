fea4ed3eea4d4710b9daea5e12ae51ea49e14914
jacobruleaux <jakeruleaux@gmail.com>
2017-10-25 10:01:55 -0700

    refactor comp to class for statefulness and api call

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text> AlbumList </Text>
-    </View>
-  );
-};
+class AlbumList extends React.Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
+  render() {
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
