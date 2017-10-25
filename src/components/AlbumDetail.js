c2dd6278c699ab9ab62bb5c0d7c3a0aea704e16e
jacobruleaux <jakeruleaux@gmail.com>
2017-10-25 14:30:21 -0700

    final version of the app

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -35,7 +35,9 @@ const AlbumDetail = ({ album }) => {
         />
       </CardSection>
       <CardSection>
-        <Button onPress={() => Linking.openURL(url)} />
+        <Button onPress={() => Linking.openURL(url)}>
+          BUY
+        </Button>  
       </CardSection>
     </Card>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = ({ onPress }) => {
+const Button = ({ onPress, children }) => {
   const { buttonStlyes, textStyle } = styles;
 
   return (
     <TouchableOpacity onPress={onPress} style={buttonStlyes}>
       <Text style={textStyle}>
-        Push
+        {children}
       </Text>
     </TouchableOpacity>
   );
