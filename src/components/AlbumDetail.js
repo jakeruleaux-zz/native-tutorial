3a39b93e45968b210e5a571d8a4c1337a3094076
jacobruleaux <jakeruleaux@gmail.com>
2017-10-25 10:40:54 -0700

    generate card comp

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumDetail = () => {
+const AlbumDetail = (props) => {
   return (
     <View>
-      <Text></Text>
+      <Text>{props.album.title}</Text>
     </View>
   );
-}
+};
 
 export default AlbumDetail;
